d749c5bceab1d4d1245658ddbc3275e1425266d2
Vlad Rebrean <vlad.rebrean@itiviti.com>
2020-10-24 14:59:41 +0300

    modificare
    Change-Id: Ibd979a20bf3b34530c648544d442e917f8ac4201

---
+++ Test1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //New
+        }
+    }
+}
